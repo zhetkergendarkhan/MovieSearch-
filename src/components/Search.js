37cccc9b15268b49af64b6ee057cc05290826dc6
Darkhan <zhetkergen.dar@gmail.com>
2021-02-22 17:03:20 +0600

    first commit

---
+++ src/components/Search.js
@@ -0,0 +1,36 @@
+import React, {useState} from "react";
+
+
+const Search = (props) => {
+
+    const [searchValue, setSearchValue] = useState("")
+
+    const handleSearchInputChanges = (e) => {
+        setSearchValue(e.target.value)
+    }
+
+    const resetInputField = () => {
+        setSearchValue("")
+    }
+
+    const callSearchFunction = (e) => {
+        e.preventDefault();
+        props.search(searchValue);
+        resetInputField();
+    }
+
+    return (
+        <div>
+            <form className="search">
+                <input
+                    value={searchValue}
+                    onChange={handleSearchInputChanges}
+                    type="text"
+                />
+                <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+            </form>
+        </div>
+    )
+}
+
+export default Search;
